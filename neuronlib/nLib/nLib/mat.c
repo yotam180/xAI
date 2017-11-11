2737daf609785f0a474a0fde1736a9ceda221817
Yotam Salmon <yotam.salmon@gmail.com>
2017-11-11 17:49:25 +0200

    Basic mat creation operations

--- neuronlib/nLib/nLib/mat.c
+++ neuronlib/nLib/nLib/mat.c
@@ -2,17 +2,96 @@
 
 #include "mat.h"
 
+/*
+Initializes a matrix with zero values
+*/
 mat * zero_mat(int h, int w)
 {
-	return NULL;
+	// Allocating a matrix
+	mat *m = (mat*)malloc(sizeof(mat));
+
+	// Failsafe allocation
+	if (m == NULL)
+		return NULL;
+
+	// Allocating data buffer
+	m->data = (double*)calloc(h * w, sizeof(double));
+
+	// Failsafe allocation
+	if (m->data == NULL)
+	{
+		free(m);
+		return NULL;
+	}
+
+	return m;
 }
 
+/*
+Allocates a new matrix without initializing it
+*/
+mat * alloc_mat(int h, int w)
+{
+	// Allocating the matrix
+	mat *m = (mat*)malloc(sizeof(mat));
+
+	// Failsafe allocation
+	if (m == NULL)
+		return NULL;
+	
+	m->h = h;
+	m->w = w;
+
+	// Allocating data buffer
+	m->data = (double*)malloc(h * w * sizeof(double));
+
+	// Failsafe allocation
+	if (m->data == NULL)
+	{
+		free(m);
+		return NULL;
+	}
+
+	return m;
+}
+
+/*
+Initializes a new matrix with preloaded data
+*/
 mat * new_mat(int h, int w, double * data)
 {
-	return NULL;
+	// Allocating a new matrix
+	mat *m = alloc_mat(h, w);
+	
+	// Failsafe allocation
+	if (m == NULL)
+		return NULL;
+
+	memcpy(m->data, data, h * w * sizeof(double));
+
+	return m;
 }
 
+/*
+Copies a matrix and returns a new one with the same values
+*/
 mat * clone_mat(mat * ref)
 {
-	return NULL;
+	return new_mat(ref->h, ref->w, ref->data);
+}
+
+void visualize_mat(mat * ref)
+{
+}
+
+void visualize_int_mat(mat * ref)
+{
+}
+
+/*
+Random access to a matrix element (by reference)
+*/
+double * el(mat * ref, int row, int col)
+{
+	return ref->data + row * ref->w + col;
 }
