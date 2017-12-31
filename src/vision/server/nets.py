1620a8f37d09c8c0e0f2ab966445c1276ccb6d2d
Yotam Salmon <yotam.salmon@gmail.com>
2017-12-31 17:34:38 +0200

    Create dataset addition (untested)

--- src/vision/server/nets.py
+++ src/vision/server/nets.py
@@ -7,7 +7,29 @@
 import database
 import db_entities
 
+import time
+
 db = db_entities.db_instance
 
 def create_dataset(dataset_name: str, positives: list, negatives: list, owner: database.db_item, subject: str, description: str) -> database.db_item:
-    pass
+    datasets = db.table("datasets", db_entities.DATASET)
+    
+    # Checking for duplicate IDs in existing datasets.
+    duplicates = len(list(datasets.query(
+        lambda c: c.get("identifier") == dataset_name
+    )))
+    if duplicates > 0:
+        return False, "Dataset ID already exists"
+
+    dataset = datasets.new() \
+        .set("identifier", dataset_name) \
+        .set("username", owner.get("username")) \
+        .set("subject", subject) \
+        .set("description", description) \
+        .set("positive_keywords", positives) \
+        .set("negative_keywords", negatives) \
+        .set("last_updated", time.time())
+
+    datasets.update(dataset)
+
+    return True, None
