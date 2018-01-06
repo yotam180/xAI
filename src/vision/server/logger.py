06ef13556ad3613daf14696850ec84be441854e6
Yotam Salmon <yotam.salmon@gmail.com>
2018-01-06 21:26:17 +0200

    Change log file location (database folder is not to be directly accessed)

--- src/vision/server/logger.py
+++ src/vision/server/logger.py
@@ -1,12 +1,16 @@
 import socket
 import json
 from time import time
-#this function checks wheather client address reqeust was recieved in the last 3 seconds(could be changed)
-# if request was recieved return true, otherwise return false
+
 MINIMAL_TIME = 3
+
 def log(clientAddr):
+    """
+    This function checks whether a client address reqeust was recieved in the last 3 seconds (parameterized).
+    If a request was recieved, returns true. Otherwise, returns false.
+    """
     valid =False
-    file = open("server/database/logs/logs.txt","r+")
+    file = open("server/logs/log.txt","r+")
     logs = json.loads(file.read())
     if(logs[clientAddr]-time()<MINIMAL_TIME):
         valid = True
