a422f2e55f878b93bcf21a37c0d81bd43d9ff129
Yotam Salmon <yotam.salmon@gmail.com>
2018-01-08 15:57:41 +0200

    Begin working on web UI

---
+++ src/vision/web/xai_web/header.php
@@ -0,0 +1,15 @@
+<script type="text/javascript">
+    $(document).ready(function() {
+        // TODO: finish
+    });
+</script>
+<div id="header">
+    <table id="header_text" border="3">
+        <tr>
+            <td>About</td>
+            <td>Products</td>
+            <td>Demo</td>
+            <td>Account</td>
+        </tr>
+    </table>
+</div>
---
+++ src/vision/web/xai_web/include.php
@@ -0,0 +1,6 @@
+<meta charset="UTF-8">
+
+<link rel="StyleSheet" type="text/css" href="style/main.css"></link>
+
+<script type="text/javascript" src="js/jquery.js"></script>
+
