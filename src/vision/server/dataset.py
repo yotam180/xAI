9b7b9994c401fbbe1c8930ed0d19e25c4b0008ec
Yotam Salmon <yotam.salmon@gmail.com>
2018-02-02 17:21:41 +0200

    Create the dataset handler skeleton

--- src/vision/server/dataset.py
+++ src/vision/server/dataset.py
@@ -12,3 +12,10 @@ from http_helper import msg, get_session, post, json_post
 
 # For communicating with the downloader thread we will use the message queue
 import task_scheduler
+
+@handler("create_dataset", "POST")
+def create_dataset_handler(req):
+    """
+    Handler for creating a dataset on the server side.
+    """
+    pass
