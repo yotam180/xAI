fe568b4f7677012f4280336d93a77b3f0286f47c
Yotam Salmon <yotam.salmon@gmail.com>
2018-02-05 20:54:46 +0200

    Return error reporting that was turned off for debugging

--- src/vision/server/dataset.py
+++ src/vision/server/dataset.py
@@ -30,54 +30,54 @@ def create_dataset_handler(req):
         return 403, {}, msg("Not Authenticated")
     
     data = json_post(req)
-    #try:
-    subject = data["subject"]
-    description = data["description"]
-    positive = data["positive"]
-    negative = data["negative"]
-    identifier = data["identifier"] if "identifier" in data else user.get("username") + "_" + subject
+    try:
+        subject = data["subject"]
+        description = data["description"]
+        positive = data["positive"]
+        negative = data["negative"]
+        identifier = data["identifier"] if "identifier" in data else user.get("username") + "_" + subject
 
-    obj = {
-        "tasks": [],
-        "done": [],
-        "ready": []
-    }
-    for w in positive:
-        if not kw.exists(w):
-            obj["tasks"].append(
-                ts.download(w)
-            )
+        obj = {
+            "tasks": [],
+            "done": [],
+            "ready": []
+        }
+        for w in positive:
+            if not kw.exists(w):
+                obj["tasks"].append(
+                    ts.download(w)
+                )
+            else:
+                obj["ready"].append(w)
+
+        for w in negative:
+            if not kw.exists(w):
+                obj["tasks"].append(
+                    ts.download(w)
+                )
+            else:
+                obj["ready"].append(w)
+
+        success, obj_name = nets.create_dataset(
+            identifier,
+            positive,
+            negative,
+            user.item_id,
+            subject,
+            description
+        )
+
+        if not success:
+            return 200, {}, msg(obj_name)
+
+        if len(obj["tasks"]) > 0:
+            pending[obj_name] = obj
+            return 200, {}, msg("Dataset Creating")
         else:
-            obj["ready"].append(w)
-
-    for w in negative:
-        if not kw.exists(w):
-            obj["tasks"].append(
-                ts.download(w)
-            )
-        else:
-            obj["ready"].append(w)
-
-    success, obj_name = nets.create_dataset(
-        identifier,
-        positive,
-        negative,
-        user.item_id,
-        subject,
-        description
-    )
-
-    if not success:
-        return 200, {}, msg(obj_name)
-
-    if len(obj["tasks"]) > 0:
-        pending[obj_name] = obj
-        return 200, {}, msg("Dataset Creating")
-    else:
-        nets.set_working(obj_name)
-        return 200, {}, msg("Dataset Created")
-    #except:
-    #    return 400, {}, msg("Content is not in the correct format or a parameter is missing")
+            nets.set_working(obj_name)
+            return 200, {}, msg("Dataset Created")
+    except:
+        return 400, {}, msg("Content is not in the correct format or a parameter is missing")
 
 
 def done_task(task):
