de41af32ee79c038184b4361e16b2331fb7caf25
Yotam Salmon <yotam.salmon@gmail.com>
2018-02-10 14:49:42 +0200

    Create the trainer skeleton

---
+++ src/vision/server/trainer.py
@@ -0,0 +1,22 @@
+#
+#   The trainer module to create classifiers based on datasets.
+#   Author: Yotam Salmon
+#   Last Edited: 10/02/2018
+#
+
+# Importing our task queues
+import task_scheduler as ts
+
+# Configuration variables
+from constants import DEBUG, IMG_SIZE
+
+def run():
+    pass
+
+def shutdown():
+
+def _work():
+    """
+    The working thread. 
+    """
+    pass
