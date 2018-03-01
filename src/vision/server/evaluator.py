95c80e5ce30c5ba602e54ed7f63eec78d7613640
Yotam Salmon <yotam.salmon@gmail.com>
2018-03-01 16:40:39 +0200

    Can now classify images in main process

--- src/vision/server/evaluator.py
+++ src/vision/server/evaluator.py
@@ -5,3 +5,49 @@
 #
 
 import nLib
+
+import cv2
+import numpy as np
+
+def process_image(img):
+    """
+    Converts an arbitary image to a matrix that can be fed into a classifier.
+    """
+    
+    # Making the image 3D
+    if len(img.shape) < 3:
+        img = img.reshape(img.shape[0], img.shape[1], 1)
+
+    # Grayscaling the image
+    if img.shape[2] > 1:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    # Cutting the center square
+    m = min(img.shape[0], img.shape[1])
+    img = img[img.shape[0] // 2 - m // 2 : img.shape[0] // 2 + m // 2, img.shape[1] // 2 - m // 2 : img.shape[1] // 2 + m // 2]
+
+    # Resizing
+    img = cv2.resize(img, (nLib.IMG_SIZE, nLib.IMG_SIZE))
+
+    return img
+
+def classify_image(img, classifier_name):
+    """
+    Classifying an image
+    """
+    try:
+        X = process_image(img)
+    except:
+        return False, "Error while trying to process the image"
+    
+    try:
+        C = nLib.load_classifier(classifier_name)
+    except:
+        return False, "Error while trying to load the classifier"
+
+    try:
+        Y = C.predict(X.reshape(-1, nLib.IMG_SIZE, nLib.IMG_SIZE, 1))
+    except:
+        return False, "Error while trying to analyze the image"
+
+    return True, Y[0][1] / np.sum(Y)
