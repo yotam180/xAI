947505e648c3bba74a31b4f22d340432c2bab744
Yotam Salmon <yotam.salmon@gmail.com>
2018-03-03 13:16:01 +0200

    Add dataset page

---
+++ src/vision/server/web/dataset.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>xAI | Dataset </title>
+
+    <?php require("include.php"); ?>
+
+    <link rel="StyleSheet" href="style/master.css" />
+
+</head>
+<body>
+
+</body>
+</html>
